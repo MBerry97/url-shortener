ab2ee378b26102055dfd257d085d8c33784ab84d
MBerry97 <imatty2011@hotmail.co.uk>
2021-08-11 22:19:19 +0100

    created circular icon component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Button from './Components/Shared/Button/Button';
+import CircularIcon from './Components/Shared/Icons/CircularIcon/CircularIcon';
 
 function App() {
   return (
     <div className="App  ">
       <Button text="shorten it" type="circular" />
       <Button text="shorten it" type="square" />
+      <CircularIcon>hi</CircularIcon>
       <header className="App-header" />
     </div>
   );
---
+++ src/Components/Shared/Icons/CircularIcon/CircularIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './CircularIcon.css';
+
+export default function CircularIcon({ children }) {
+  return <div className="circularIcon">{children}</div>;
+}
