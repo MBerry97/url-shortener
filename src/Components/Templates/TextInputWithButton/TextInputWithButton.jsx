3ee3aa3594c3cf2326b23cd0fb969752198e5869
MBerry97 <imatty2011@hotmail.co.uk>
2021-08-17 17:19:04 +0100

    added bg image to text input with button

--- src/Components/Templates/TextInputWithButton/TextInputWithButton.jsx
+++ src/Components/Templates/TextInputWithButton/TextInputWithButton.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from '../../Shared/Button/Button';
 import './TextInputWithButton.css';
+import mobileShortenBg from '../../../assets/bg-shorten-mobile.svg';
 
 export default function TextInputWithButton({ onSubmit }) {
   return (
     <div className="textInputWithButton_container">
+      <img src={mobileShortenBg} alt="backgroun illustration" />
       <form onSubmit={(e) => onSubmit(e)}>
-        <input type="text" />
+        <input type="text" placeholder="Shorten a link here..." />
         <Button type="square" text="Shorten it!" btnType="submit" />
       </form>
     </div>
