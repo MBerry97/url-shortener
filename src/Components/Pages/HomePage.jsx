09e29e6d47b2a220686ee3e45092bdcac5eddaf3
MBerry97 <imatty2011@hotmail.co.uk>
2021-09-06 17:42:51 +0100

    added footer to page layout

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './HomePage.css';
 import Header from '../Layout/Header/Header';
 import Main from '../Layout/Main/Main';
+import Footer from '../Layout/Footer/Footer';
 
 export default function HomePage() {
   return (
     <div className="homePage_container">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
