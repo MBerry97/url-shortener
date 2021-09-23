17f0edbe3620077cc39fadb415581e79ad34c8f4
MBerry97 <imatty2011@hotmail.co.uk>
2021-09-23 16:45:14 +0100

    finished html and css of the footer

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import './Footer.css';
+import logo from '../../../assets/logo-white.svg';
+import facebookLogo from '../../../assets/icon-facebook.svg';
+import twitterLogo from '../../../assets/icon-twitter.svg';
+import pinterestLogo from '../../../assets/icon-pinterest.svg';
+import instagramLogo from '../../../assets/icon-instagram.svg';
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <span>footer</span>
+      <img src={logo} alt="logo" />
+
+      <span className="footerSpan_heading">Features</span>
+      <span className="footerSpan_subHeading">Link shortening</span>
+      <span className="footerSpan_subHeading">Branded Links</span>
+      <span className="footerSpan_subHeading">Analytics</span>
+
+      <span className="footerSpan_heading">Resources</span>
+      <span className="footerSpan_subHeading">Blog</span>
+      <span className="footerSpan_subHeading">Developers</span>
+      <span className="footerSpan_subHeading">Support</span>
+
+      <span className="footerSpan_heading">Company</span>
+      <span className="footerSpan_subHeading">About</span>
+      <span className="footerSpan_subHeading">Our Team</span>
+      <span className="footerSpan_subHeading">Careers</span>
+      <span className="footerSpan_subHeading">Contact</span>
+
+      <div className="footerIcon_container">
+        <img src={facebookLogo} alt="facebook" />
+        <img src={twitterLogo} alt="twitter" />
+        <img src={pinterestLogo} alt="pinterest" />
+        <img src={instagramLogo} alt="instagram" />
+      </div>
     </footer>
   );
 }
