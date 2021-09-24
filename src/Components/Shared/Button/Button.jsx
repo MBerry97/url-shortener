9c89e12d793d9eb3ff8e9f651049e797fb8d597c
MBerry97 <imatty2011@hotmail.co.uk>
2021-09-24 15:53:10 +0100

    click on copy button writes link to clipboard

--- src/Components/Shared/Button/Button.jsx
+++ src/Components/Shared/Button/Button.jsx
@@ -2,9 +2,14 @@
 import React from 'react';
 import './Button.css';
 
-export default function Button({ text, type, btnType, id }) {
+export default function Button({ text, type, btnType, id, onClickFn }) {
   return (
-    <button type={btnType} className={`button button--${type}`} id={id}>
+    <button
+      type={btnType}
+      className={`button button--${type}`}
+      id={id}
+      onClick={onClickFn ? () => onClickFn() : null}
+    >
       {text}
     </button>
   );
--- src/Components/Templates/LinkBox/LinkBox.jsx
+++ src/Components/Templates/LinkBox/LinkBox.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../Shared/Button/Button';
 import './LinkBox.css';
 
 function LinkBox({ newLink, oldLink }) {
+  const [copied, setCopied] = useState(false);
+  const handleButtonClick = () => {
+    if (copied) {
+      navigator.clipboard.writeText('');
+    } else {
+      navigator.clipboard.writeText(newLink);
+    }
+    setCopied((prev) => !prev);
+  };
+
   return (
     <div className="linkBox">
       <span>{oldLink}</span>
       <span>{newLink}</span>
-      <Button type="square" text="Copy" id="linkBox_btn" />
+      {!copied && (
+        <Button
+          type="square"
+          text="Copy"
+          id="linkBox_btn"
+          onClickFn={handleButtonClick}
+        />
+      )}
+      {copied && (
+        <Button
+          type="active"
+          text="Copied!"
+          id="linkBox_btn"
+          onClickFn={handleButtonClick}
+        />
+      )}
     </div>
   );
 }
